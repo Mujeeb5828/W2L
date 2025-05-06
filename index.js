633890fb4f8a4dfa65f5cd9408d227ef657db870
Mujeeb <mujeeburrahman1999@gmail.com>
2025-05-06 20:19:41 +0530

    recaptcha validation 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function beforesubmit() {
-  let inputdate = document.querySelector(".inputdate");
-  let outputdate = document.querySelector(".outputdate");
-  console.log("1" + inputdate.value);
-  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
-  console.log(formatteddate);
-  outputdate.value = formatteddate; //test
+let captchaFlag = false;
+function beforesubmit(event) {
+  if (captchaFlag) {
+    let inputdate = document.querySelector(".inputdate");
+    let outputdate = document.querySelector(".outputdate");
+    console.log("1" + inputdate.value);
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+    console.log(formatteddate);
+    outputdate.value = formatteddate; //test
+  }else{
+    alert("Please chekc the recaptcha box to submit the form.");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
@@ -19,3 +25,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchaSuccess() {
+  captchaFlag = true;
+}
