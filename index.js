9b2647d2caed34faf8c7ad4080e0f78ea996dd61
Mujeeb <mujeeburrahman1999@gmail.com>
2025-05-06 20:07:57 +0530

    Google Recaptcha

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-function beforesubmit(){
-    let inputdate = document.querySelector(".inputdate");
-    let outputdate = document.querySelector(".outputdate");
-    console.log('1'+inputdate.value);
-    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
-    console.log(formatteddate);
-    outputdate.value=formatteddate; //test
+function beforesubmit() {
+  let inputdate = document.querySelector(".inputdate");
+  let outputdate = document.querySelector(".outputdate");
+  console.log("1" + inputdate.value);
+  let formatteddate = new Date(inputdate.value).toLocaleDateString("en-US");
+  console.log(formatteddate);
+  outputdate.value = formatteddate; //test
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
